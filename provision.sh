126a6b78ae6e4629d0fc4d57b05ee8932f84ad22
alexsedova <als@praqma.net>
2016-09-29 11:25:06 +0200

    Commit scripts

---
+++ provision.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -u # Variables must be explicit
+set -e # If any command fails, fail the whole thing
+set -o pipefail
+
+# Make sure SSH knows to use the correct pem. 
+# Set here your pem file to connect to aws 
+ssh-add aws.pem
+ssh-add -l
+# Load the AWS keys.
+# Create aws_keys file. In the file you will export AWS keys in the environment
+# May looks like:
+# export AWS_ACCESS_KEY_ID='YOURKEYS'
+# export AWS_SECRET_ACCESS_KEY='YOURSECRETKEYS'
+# export EC2_REGION='us-west-2'
+. ./inventory/aws_keys
+
+# Start a new instance
+ansible-playbook playbook/site.yml -vv
